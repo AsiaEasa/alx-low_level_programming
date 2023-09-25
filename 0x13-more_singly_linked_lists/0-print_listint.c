e4786eee43d812f9be68f76f3b3c5628ae30ac09
AsiaEasa <asiaeasa89@gmail.com>
2023-09-25 10:04:10 -0700

    alx-low_level_programming

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "list.h"
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
 
 size_t print_listint(const listint_t *h)
 {int l;
 	if (h == NULL)
 		printf("the list is empty");
 	else
-		printf("%d\n", h->n);
-	h = h->next;
-	l++;
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			l++;}
+	return (l);
 }
