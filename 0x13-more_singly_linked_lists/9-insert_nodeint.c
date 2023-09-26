9cabec2a4b2da6f4125321ad0ec8cf1990a683a9
AsiaEasa <asiaeasa89@gmail.com>
2023-09-25 22:01:58 -0700

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	tmp = malloc(sizeof(listint_t));
 
-	if (!tmp || !ptr)
+	if (!tmp)
 		return (NULL);
 
 	tmp->n = n;
