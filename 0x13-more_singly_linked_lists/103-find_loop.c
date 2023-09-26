b627161f29aa9b3677e312a892f9205a1b89ddd3
AsiaEasa <asiaeasa89@gmail.com>
2023-09-26 01:58:27 -0700

    done

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -9,15 +9,19 @@ listint_t *find_listint_loop(listint_t *head)
 {int i = 0;
 	listint_t *tmp = head;
 	listint_t *ptr = head;
+
 	if (!head)
 		return (NULL);
+
 	for (; tmp && ptr && ptr->next; i++)
 	{
 		ptr = ptr->next->next;
 		tmp = tmp->next;
+
 		if (ptr == tmp)
 		{
-			ymp = head;
+			tmp = head;
+
 			while (tmp != ptr)
 			{
 				tmp = tmp->next;
