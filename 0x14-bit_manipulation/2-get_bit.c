8947bd06b100b85b3358eeea8cb8ed0a28efb506
AsiaEasa <asiaeasa89@gmail.com>
2023-09-28 05:28:45 -0700

    done

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i, a[100];
+	for (i = 0; n > 0; i++)    
+	{    
+		a[i] = n % 2;    
+		n = n / 2;    
+	}    
+	printf("%d",a[index]);
+	return (0);
+}
