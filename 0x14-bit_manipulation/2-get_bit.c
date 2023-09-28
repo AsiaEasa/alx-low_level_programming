398f96c6a7e1530f51ddd42edd18d15b75249394
AsiaEasa <asiaeasa89@gmail.com>
2023-09-28 09:13:45 -0700

    done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int i, a[100];
-	for (i = 0; n > 0; i++)    
-	{    
-		a[i] = n % 2;    
-		n = n / 2;    
-	}    
-	printf("%d",a[index]);
+
+	for (i = 0; n > 0; i++)
+	{
+		a[i] = n % 2;
+		n = n / 2;
+	}
+	printf("%d", a[index]);
 	return (0);
 }
