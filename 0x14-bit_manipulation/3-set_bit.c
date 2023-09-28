dfea43f1e7902229ec144c7321e48ee96341d520
AsiaEasa <asiaeasa89@gmail.com>
2023-09-28 11:25:57 -0700

    done

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,27 +9,12 @@
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
-{int rem, int_num = 0, base = 1;
-	unsigned int i, x, a[100] __attribute__((unused));
+{
 
 	if (index > 63)
 		return (-1);
 
-	for (i = 0; *n > 0; i++)
-	{
-		a[i] = *n % 2;
-		*n = *n / 2;
+	*n |= (1UL << index);
 
-		if (i == index)
-			a[i] = 1;
-	}
-
-	for (x = 0; x <= i; x++)
-	{rem = a[i] % 10;
-		int_num = int_num + rem * base;
-		a[i] = a[i] / 10;
-		base = base * 2;
-	}
-	*n = int_num;
 	return (1);
 }
