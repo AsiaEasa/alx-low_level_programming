d20ff42f425ace509cd55793e693a57846a58329
AsiaEasa <asiaeasa89@gmail.com>
2023-09-29 08:48:47 -0700

    done

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,10 +5,6 @@
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
-{int get_endianness(void)
-	{
-		int value = 1;
-		char* ptr = (char *)&value;
-
-		return ((int *)*ptr);
-	}
+{
+	return ;
+}
