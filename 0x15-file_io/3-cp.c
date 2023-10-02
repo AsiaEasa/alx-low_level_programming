f6577b2a2a2b6ee2788ecdc987fb8848dd50cef1
AsiaEasa <asiaeasa89@gmail.com>
2023-10-02 14:18:49 -0700

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,7 +13,7 @@
  *
  * Return: 0 on success, or an error code (97, 98, 99, or 100) on failure.
  *
- * Description:
+ * Description:3-cp.c
  * This program takes two command-line arguments: the source file (file_from)
  * and the destination file (file_to). It copies the content of the source
  * file to the destination file while handling various error conditions.
@@ -24,54 +24,49 @@
  *  - 99: Failed to write to the destination file.
  *  - 100: Failed to close a file descriptor.
  */
-void error(const char *Mes, int exit)
+void ex_error(const char *Mes, int _exit)
 {
 	dprintf(STDERR_FILENO, "Error: %s\n", Mes);
-	exit(exit);
+	exit(_exit);
 }
 
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
-		error("Usage: cp file_from file_to", 97);
-
+		ex_error("Usage: cp file_from file_to", 97);
 	const char *file_from = argv[1];
 	const char *file_to = argv[2];
-
 	int ptr_from = open(file_from, O_RDONLY);
 
 	if (ptr_from == -1)
-		error("Can't read from file", 98);
-
+		ex_error("Can't read from file", 98);
 	int ptr_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (ptr_to == -1)
 	{
 		close(ptr_from);
-		error("Can't write to file", 99);
+		ex_error("Can't write to file", 99);
 	}
-
 	char buf[SIZE];
 	ssize_t r, w;
 
 	r = read(ptr_from, buf, SIZE);
-	w = write(ptr_to, buf, r)
-		if (w != r)
-		{
-			close(ptr_from);
-			close(ptr_to);
-			error("Can't write to file", 99);
-		}
+	w = write(ptr_to, buf, r);
+	if (w != r)
+	{
+		close(ptr_from);
+		close(ptr_to);
+		ex_error("Can't write to file", 99);
+	}
 
 	if (r == -1)
 	{
 		close(ptr_from);
 		close(ptr_to);
-		error("Can't read from file", 98);
+		ex_error("Can't read from file", 98);
 	}
 
 	if (close(ptr_from) == -1 || close(ptr_to) == -1)
-		error("Can't close ptr", 100);
-
+		ex_error("Can't close ptr", 100);
 	return (0);
 }
