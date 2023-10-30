d8f14f093bfd87ed8345e5f763076203b0ab7a19
AsiaEasa <asiaeasa89@gmail.com>
2023-10-30 12:25:58 -0700

    done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,78 +1,77 @@
-#include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-void close_elf(int elf);
-void check_elf(unsigned char *e_ident);
-/**
- * close_elf - Closes an ELF file.
- * @elf: The file descriptor of the ELF file.
- *
- * Description: If the file cannot be closed - exit code 98.
- */
-void close_elf(int elf)
-{
-	if (close(elf) == -1)
-		exit(9);
-}
-/**
- * check_elf - Checks if a file is an ELF file.
- * @e_ident: A pointer to an array containing the ELF magic numbers.
- *
- * Description: If the file is not an ELF file - exit code 98.
- */
-void check_elf(unsigned char *e_ident)
-{
-	int index;
+#include <elf.h> 
+ #include <sys/types.h> 
+ #include <sys/stat.h> 
+ #include <fcntl.h> 
+ #include <unistd.h> 
+ #include <stdio.h> 
+ #include <stdlib.h> 
+ 
+/** 
+  * close_e - Closes an ELF file. 
+  * @elf: The file descriptor of the ELF file. 
+  * 
+  * Description: If the file cannot be closed - exit code 98. 
+  */ 
+ void close_e(int elf) 
+ { 
+         if (close(elf) == -1) 
+                 exit(98); 
+ } 
+ /** 
+  * check_e - Checks if a file is an ELF file. 
+  * @e_ident: A pointer to an array containing the ELF magic numbers. 
+  * 
+  * Description: If the file is not an ELF file - exit code 98. 
+  */ 
+ void check_e(unsigned char *e_ident) 
+ { 
+         int x; 
+  
+         for (x = 0; x < 4; x++) 
+         { 
+                 if (e_ident[x] != 127 && 
+                                 e_ident[x] != 'E' && 
+                                 e_ident[x] != 'L' && 
+                                 e_ident[x] != 'F') 
+  
+                 { 
+                         dprintf(STDERR_FILENO, "Not an ELF file\n"); 
+                         exit(98); 
+                 } 
+         } 
+ } 
 
-	for (index = 0; index < 4; index++)
-	{
-		if (e_ident[index] != 127 &&
-				e_ident[index] != 'E' &&
-				e_ident[index] != 'L' &&
-				e_ident[index] != 'F')
-
-		{
-			dprintf(STDERR_FILENO, "Not an ELF file\n");
-			exit(98);
-		}
-	}
-}
-
-
-/**
- * main - Displays the information contained in the
- * ELF header at the start of an ELF file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: 0 on success.
- *
- * Description: If the file is not an ELF File or
- * the function fails - exit code 98.
- */
-int main(int __attribute__((__unused__)) argc, char *argv[])
-{
-	Elf64_Ehdr *header;
-	int o, r;
-	(void)r;
-
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		exit(98);
-	}
-	header = malloc(sizeof(Elf64_Ehdr));
-	r = read(o, header, sizeof(Elf64_Ehdr));
-
-	check_elf(header->e_ident);
-	printf("ELF Header:\n");
-
-	free(header);
-	close_elf(o);
-	return (0);
-}
+ /** 
+  * main - Displays the information contained in the 
+  * ELF der at the start of an ELF file. 
+  * @ac: The number of arguments supplied to the program. 
+  * @av: An array of pointers to the arguments. 
+  * 
+  * Return: 0 on success. 
+  * 
+  * Description: If the file is not an ELF File or 
+  * the function fails - exit code 98. 
+  */ 
+ int main(int ac, char **av) 
+ { 
+         Elf64_Ehdr *der; 
+         int op, re; 
+         (void)re; 
+         (void)ac; 
+  
+         op = open(av[1], O_RDONLY); 
+         if (op == -1) 
+         { 
+                 dprintf(STDERR_FILENO, "Error: Can't read file %s\n", av[1]); 
+                 exit(98); 
+         } 
+         der = malloc(sizeof(Elf64_Ehdr)); 
+         re = read(op, der, sizeof(Elf64_Ehdr)); 
+  
+         check_e(der->e_ident); 
+         printf("ELF Header:\n"); 
+  
+         free(der); 
+         close_e(op); 
+         return (0); 
+ }
