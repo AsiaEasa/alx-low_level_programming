1abf3af174a68450ecc37ba8a618f263bbd13b74
AsiaEasa <asiaeasa89@gmail.com>
2023-10-30 08:50:20 -0700

    done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -208,22 +208,13 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		exit(98);
 	}
 	header = malloc(sizeof(Elf64_Ehdr));
-	if (header == NULL)
-	{
-		close_elf(o);
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		exit(98);
-	}
 	r = read(o, header, sizeof(Elf64_Ehdr));
 
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
-	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
 	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);
