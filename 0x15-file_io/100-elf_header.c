6e1e9ff481754895150ef13068aba52abcbc787c
AsiaEasa <asiaeasa89@gmail.com>
2023-10-30 09:11:38 -0700

    done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -42,28 +42,7 @@ void check_elf(unsigned char *e_ident)
 		}
 	}
 }
-
 /**
- * print_type - Prints the type of an ELF header.
- * @e_type: The ELF type.
- * @e_ident: A pointer to an array containing the ELF class.
- */
-void print_type(unsigned int e_type, unsigned char *e_ident)
-{
-	if (e_ident[EI_DATA] == ELFDATA2MSB)
-		e_type >>= 8;
-
-	printf(" Type: ");
-
-	switch (e_type)
-	{
-		case ET_NONE:
-			printf("NONE (None)\n");
-			break;
-	}
-}
-/**
- * main - Displays the information contained in the
  * ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
@@ -91,6 +70,5 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	check_elf(header->e_ident);
 
 	free(header);
-	close_elf(o);
 	return (0);
 }
