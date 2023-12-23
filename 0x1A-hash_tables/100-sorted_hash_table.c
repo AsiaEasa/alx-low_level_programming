b22e45f8096678c9557f82a8451d6c2de1ede5b8
AsiaEasa <asiaeasa89@gmail.com>
2023-12-22 22:53:46 -0800

    done

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * sortedlist - function for insert new node in sorted
+ * sorted_list - function for insert new node in sorted
  * @ht: pointer to sorted hash table
  * @new_node: pew node to insert
  *
