2aeed12e1691448fa3b6f180a8925b11da850e21
AsiaEasa <asiaeasa89@gmail.com>
2024-04-10 11:32:16 -0700

    done

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,21 +13,22 @@ int jump_search(int *array, size_t size, int value)
 {
 	int i, S, P;
 
-	if (!array|| size == 0)
+	if (!array || size == 0)
 		return (-1);
 
 	S = sqrt(size);
 	P = 0;
 
 	while (array[P] < value && P < (int)size)
-        {
+	{
 		printf("Value checked array[%d] = [%d]\n", P, array[P]);
 		P += S;
 	}
 
 	printf("Value found between indexes [%d] and [%d]\n", P - S, P);
 
-	for (i = P - S; i <= P && i < (int)size; ++i) {
+	for (i = P - S; i <= P && i < (int)size; ++i)
+	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
