82b028b9e6408268e9288434bb691baca6b455d5
AsiaEasa <asiaeasa89@gmail.com>
2024-04-10 12:02:25 -0700

    done

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,12 +10,12 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	if (!array || size == 0)
-		return (-1);
-
 	size_t low = 0;
 	size_t high = size - 1;
 	size_t pos;
+
+	if (!array || size == 0)
+		return (-1);
 
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
