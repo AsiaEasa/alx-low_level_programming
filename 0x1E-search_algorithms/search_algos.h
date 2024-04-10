55233c77e63fe7ec28c5609e45d7b712c448a2f9
AsiaEasa <asiaeasa89@gmail.com>
2024-04-10 11:59:33 -0700

    done

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,6 +2,9 @@
 #define SEARCH_H
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int interpolation_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
